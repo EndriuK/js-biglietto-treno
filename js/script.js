27df11599958c31ee0d9b96c6ea6cd732ce9bbc4
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-06-25 15:46:22 +0200

    [UPD] dichiarazione sconti

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 let eta = prompt("Inserisci la tua età");
 let chilometri = prompt("Inserisci chilometri da percorrere");
 
-if (eta < 18) {
 
+let sconto =  0
+
+
+if (eta < 18) {
+    sconto = 20;
 } 
 
 else if (eta > 65) {
-
+    sconto = 40;
 }
 
-else {
-    
-}
 
