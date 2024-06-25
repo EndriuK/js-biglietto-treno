1453352d98953ff1125ef88f711737a628b88024
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-06-25 15:54:44 +0200

    [UPD] aggiunta varie variabili

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 let eta = prompt("Inserisci la tua età");
 let chilometri = prompt("Inserisci chilometri da percorrere");
 
-
+let prezzoBiglietto = 0.21 * chilometri
 let sconto =  0
-
+let prezzoScontato
+let prezzoFinale
 
 if (eta < 18) {
     sconto = 20;
@@ -13,4 +14,7 @@ else if (eta > 65) {
     sconto = 40;
 }
 
+if (sconto != 0 ) {
+
+}
 
